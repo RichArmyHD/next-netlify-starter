ef770b4d3d6417384505e7c47ddcbca9c8b4bc60
RichArmyHD <66500782+RichArmyHD@users.noreply.github.com>
2022-06-24 14:08:13 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,44 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    This is the array that will hold the todo list items
+let todoItems = [];
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+// This function will create a new todo object based on the
+// text that was entered in the text input, and push it into
+// the `todoItems` array
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
 
-      <Footer />
-    </div>
+  todoItems.push(todo);
+  console.log(todoItems);
+}
+
+// Select the form element
+const form = document.querySelector('.js-form');
+// Add a submit event listener
+form.addEventListener('submit', event => {
+  // prevent page refresh on form submission
+  event.preventDefault();
+  // select the text input
+  const input = document.querySelector('.js-todo-input');
+
+  // Get the value of the input and remove whitespace
+  const text = input.value.trim();
+  if (text !== '') {
+    addTodo(text);
+    input.value = '';
+    input.focus();
+  }
+});
+const todo = {
+  text,
+  checked: false,
+  id: Date.now(),
+};
   )
 }
